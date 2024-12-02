4b3650121a331706086135c954936df4b24d8cbc
fheshiki <70349016+paches00@users.noreply.github.com>
2024-12-02 18:59:05 +0100

    fix dns

--- ubuntu_enable_local_dns.sh
+++ ubuntu_enable_local_dns.sh
@@ -4,7 +4,7 @@ cp /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.co
 config=$(cat /etc/NetworkManager/NetworkManager.conf)
 if [[ $config == *dns=default* ]]; then
     line_number=$(grep -n "\[main\]" /etc/NetworkManager/NetworkManager.conf | cut -d: -f1)
-    sed -i "$line_number a dns=10.205.10.3" /etc/NetworkManager/NetworkManager.conf
+    sed -i "$line_number a dns=10.205.10.2" /etc/NetworkManager/NetworkManager.conf
 fi
 cp /etc/resolv.conf /etc/resolv.conf.bak
 rm /etc/resolv.conf
