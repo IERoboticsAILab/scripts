dadf750f8a0e850694e33d83b0cf2046b773b76c
fheshiki <70349016+paches00@users.noreply.github.com>
2024-12-02 20:05:32 +0100

    fixed DNS script

--- ubuntu_enable_local_dns.sh
+++ ubuntu_enable_local_dns.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
+
+# Disable and stop systemd-resolved
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
+
+# Backup NetworkManager.conf
 cp /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
-config=$(cat /etc/NetworkManager/NetworkManager.conf)
-if [[ $config == *dns=default* ]]; then
+
+# Update DNS setting in NetworkManager.conf
+if ! grep -q "dns=10.205.10.2" /etc/NetworkManager/NetworkManager.conf; then
     line_number=$(grep -n "\[main\]" /etc/NetworkManager/NetworkManager.conf | cut -d: -f1)
     sed -i "$line_number a dns=10.205.10.2" /etc/NetworkManager/NetworkManager.conf
 fi
+
+# Backup and remove resolv.conf
 cp /etc/resolv.conf /etc/resolv.conf.bak
-rm /etc/resolv.conf
+rm -f /etc/resolv.conf
+
+# Restart NetworkManager
 sudo systemctl restart NetworkManager
