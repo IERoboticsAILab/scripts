7d47fbb99a010bbf9409d27eb5adce98ecc963ea
jesuisadmin <jesuisadmin@robotics>
2025-05-15 18:31:48 +0200

    Fix assert()

--- common/useful.sh
+++ common/useful.sh
@@ -2,8 +2,15 @@
 # By default, assert breaks execution if test fails
 # if _assert_wait = 1, assert waits for user to press key before continuing
 assert(){
-    if [ ${_assert_wait} -eq 1 ]; then
+    if [ "${_assert_wait}" == "1" ]; then
 	echo "Press enter..."
 	read
     fi
+}    
+
+# Print message to stderr and exit (status code 1)
+# $1: message to print
+die() {
+    echo "$1" >&2
+    exit 1
 }
